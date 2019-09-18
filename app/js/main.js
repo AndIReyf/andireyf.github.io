f48f63e184ffa6e29a7e24ba38504dc4194bcf66
AndIReyf <andrey.prudius@hotmail.com>
2019-09-19 00:00:16 +0200

    Added Products Slider and Rate Stars

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
 $(function () {
   
   var mixer = mixitup('.product__inner-box');
+
+  $(".rate-star").rateYo({
+    rating: 5,
+    fullStar: true,
+    starWidth: "12px",
+    readOnly: true
+  });
+
+  $('.product-slider__inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: false
+  });
   
 });
