209971993586e37df7aa1c64c324fafdbc56f7e6
AndIReyf <andrey.prudius@hotmail.com>
2019-10-08 21:35:37 +0200

    Added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -18,7 +18,32 @@ $(function () {
     slidesToShow: 4,
     slidesToScroll: 4,
     dots: true,
-    arrows: false
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1901,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 981,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
   });
 
   $('.js-range-slider').ionRangeSlider({
